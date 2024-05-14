46542d174e8d6b873daa868d924c6c420c141376
Darío Poves <119043463+dapoves@users.noreply.github.com>
2024-05-14 08:59:43 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://127.0.0.1:3000''], 
+    // tocar esto
 
     'allowed_origins_patterns' => [],
 
