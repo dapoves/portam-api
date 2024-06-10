7391634ca73f9370bf8732dcf4dc733c95415a78
dapoves <dariopoves@gmail.com>
2024-06-10 16:19:48 +0000

    Added local origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://portam.vercel.app'], 
+    'allowed_origins' => ['https://portam.vercel.app', 'http://localhost:3000'], 
 
     'allowed_origins_patterns' => [],
 
